4b1fcbcfb0b6a77015ed3fe8b39c1c91955d01e0
Alexsey Panov <panovas@vtb.ru>
2024-02-25 23:42:47 +0300

    2024-02-25

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите количество участников: ");
+        int playersNumber = scanner.nextInt();
+
+        List<String> words = readWordsFromFile("words.txt");
+
+        // если слов меньше, чем участников, то выведите сообщение:
+        // "Недостаточно слов в файле. Добавьте слова и обновите файл."
+        // и завершите выполнение программы
+
+        if (words.size() < playersNumber) {
+            System.out.println("Недостаточно слов в файле. Добавьте слова и обновите файл.");
+            return;
+        }
+        // воспользуйтесь статическим методом Collections.shuffle(List<?> list),
+        // чтобы поменять порядок слов случайным образом
+        Collections.shuffle(words);
+
+        int wordsNumber = words.size() / playersNumber;
+
+        for (int i = 0; i < playersNumber; i++) {
+            String filename = String.format("player%s.txt", i + 1);
+            List<String> subList = words.subList(i * wordsNumber, (i + 1) * wordsNumber);
+
+            writeListToFile(subList, filename);
+        }
+
+        System.out.println("Карточки готовы!");
+    }
+
+    private static List<String> readWordsFromFile(String filename) {
+        // добавьте построчное чтение из файла с помощью BufferedReader
+        // в случае ошибки выведите сообщение: "Произошла ошибка во время чтения файла."
+        ArrayList<String> strs = new ArrayList<>();
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(filename, StandardCharsets.UTF_8))) {
+            while (fileReader.ready()) {
+                strs.add(fileReader.readLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка во время чтения файла.");
+        }
+        return strs;
+    }
+
+    private static void writeListToFile(List<String> list, String filename) {
+        try (FileWriter fileWriter = new FileWriter(filename,StandardCharsets.UTF_8)) {
+            for (String val: list) {
+                fileWriter.write(val+"\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка во время записи файла.");
+        }
+
+    }
+}
